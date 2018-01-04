a5ddb44899c9465232dd91fd90d3ed782d38000c
Ashiri <ajaoiyanu@gmail.com>
2018-01-04 02:30:38 +0100

    Modified to materialize

---
+++ material.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
+
+  });
+
+
+
+
+
+
